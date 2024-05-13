e1aebab922351ac56ed930916b3734883429b1cd
andrecas <andrecas@bancolombia.com.co>
2024-05-13 17:26:00 -0500

    The general structure for the project was created.

---
+++ applications/app-service/src/main/java/co/com/ludi/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.ludi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.ludi.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
